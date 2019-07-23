87a36e122aef2a7a20ff5757dfbff5a3953ea52b
fortheskyking <15821850193@139.com>
2019-07-23 15:07:06 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,54 @@
 # -*- coding: utf-8 -*-
+
 from flask import Flask
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy # 导入扩展类
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123@127.0.0.1:3306/flask_test' #Mysql
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app) # 初始化扩展，传入程序实例 app	
+
 	
-name = 'Sky'
-movies = [
+	
+@app.route('/')
+def hello():
+	return 'Welcome to My Watchlist!'
+	
+@app.route('/')
+@app.route('/index')
+def index():
+	user = User.query.first() # 读取用户记录
+	movies = Movie.query.all() # 读取所有电影记录
+	return render_template('index.html', user=user, movies=movies)
+	
+class User(db.Model): # 表名将会是 user（自动生成，小写处理）
+	id = db.Column(db.Integer, primary_key=True) # 主键
+	name = db.Column(db.String(20)) # 名字
+
+class Movie(db.Model): # 表名将会是 movie
+	id = db.Column(db.Integer, primary_key=True) # 主键
+	title = db.Column(db.String(60)) # 电影标题
+	year = db.Column(db.String(4)) # 电影年份
+
+
+import click
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.') # 设置选项
+def initdb(drop):
+	#Initialize the database.
+	if drop: # 判断是否输入了选项
+		db.drop_all()
+	db.create_all()
+	click.echo('Initialized database.') # 输出提示信息
+	
+@app.cli.command()
+def forge():
+#Generate fake data."""
+	db.create_all()
+# 全局的两个变量移动到这个函数内
+	name = 'fortheskyking'
+	movies = [
 	    {'title': 'My Neighbor Totoro', 'year': '1988'},
 	    {'title': 'Dead Poets Society', 'year': '1989'},
 	    {'title': 'A Perfect World', 'year': '1993'},
@@ -16,13 +60,10 @@ movies = [
 	    {'title': 'WALL-E', 'year': '2008'},
 	    {'title': 'The Pork of Music', 'year': '2012'},
 	]
-	
-	
-@app.route('/')
-def hello():
-	return 'Welcome to My Watchlist!'
-	
-@app.route('/')
-@app.route('/index')
-def index():
-	return render_template('index.html', name=name, movies=movies)
+	user = User(name=name)
+	db.session.add(user)
+	for m in movies:
+		movie = Movie(title=m['title'], year=m['year'])
+		db.session.add(movie)
+	db.session.commit()
+	click.echo('Done.')
